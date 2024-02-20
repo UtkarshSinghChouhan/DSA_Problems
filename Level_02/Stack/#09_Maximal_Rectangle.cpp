5c7e0082c08ff09bd185ba50ca0dd65d3b80aaac
UtkarshSinghChouhan <uscutkarsh9@gmail.com>
2024-02-20 07:46:45 +0530

    stack level 2

--- Level_02/Stack/#09_Maximal_Rectangle.cpp
+++ Level_02/Stack/#09_Maximal_Rectangle.cpp
@@ -37,19 +37,30 @@ int largestAreaHistogram(vector<int> &arr) {
 }
 
 int maximalRectangle(vector<vector<int>> &mat) {
-    int area = 0, n = mat.size(), m = mat[0].size();
-    vector<int> arr(n, 0);
+    int n = mat.size(), m = mat[0].size();
+    vector<int> arr(m, 0);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < m; i++) {
+        arr[i] = mat[0][i];
+    }
+
+    int area = largestAreaHistogram(arr);
+
+    for (int i = 1; i < n; i++) {
         for (int j = 0; j < m; j++) {
             if (mat[i][j] == 1) {
-                arr[i] += 1;
+                arr[j] += 1;
             } else {
-                arr[i] = 0;
+                arr[j] = 0;
             }
         }
     }
 
+    for (auto &val : arr) {
+        cout << val << " ";
+    }
+
+    cout << endl;
     int val = largestAreaHistogram(arr);
     area = max(area, val);
     return area;
---
+++ Level_02/Stack/#10_Validate_Stack_Sequence.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+bool validateStackSequence(vector<int> &pushed, vector<int> &popped) {
+    int n = pushed.size();
+    int i = 0, j = 0;
+    stack<int> st;
+
+    while (i < n) {
+        st.push(pushed[i]);
+        i++;
+
+        while (!st.empty() && j < n && st.top() == popped[j]) {
+            st.pop();
+            j++;
+        }
+    }
+
+    return st.empty();
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> pushed;
+    vector<int> popped;
+
+    for (int i = 0; i < n; i++) {
+        int val;
+        cin >> val;
+        pushed.push_back(val);
+    }
+
+    for (int i = 0; i < n; i++) {
+        int val;
+        cin >> val;
+        popped.push_back(val);
+    }
+
+    if (validateStackSequence(pushed, popped)) {
+        cout << "true";
+    } else {
+        cout << "false";
+    }
+
+    return 0;
+}
